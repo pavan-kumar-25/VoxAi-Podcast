f49c764af9a80c3a268cdc804720b09c06205124
pavan-kumar-25 <pavankumar.c@mca.christuniversity.in>
2024-07-11 19:07:59 +0530

    update

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mt-9 gap-9'>
+      <h1 className=' text-white-1 ml-9'>Hello world</h1>
+    </div>
+  )
+}
+
+export default Home
