e8b6ed35f90d53e73bcefe2df37470ed2cdb5e30
pavan-kumar-25 <pavankumar.c@mca.christuniversity.in>
2024-08-21 11:57:01 +0530

    update new

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,8 @@
 import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
 import MobileNav from "@/components/MobileNav";
-// import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
-// import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster"
 // import PodcastPlayer from "@/components/PodcastPlayer";
 
 export default function RootLayout({
@@ -28,7 +27,7 @@ export default function RootLayout({
               <MobileNav />
             </div>
             <div className="flex flex-col md:pb-14">
-              {/* <Toaster /> */}
+              <Toaster />
 
               {children}
             </div>
