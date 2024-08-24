724219fa8c78110d322a26e2b02c8c7b6a93aaf7
pavan-kumar-25 <pavankumar.c@mca.christuniversity.in>
2024-08-24 09:43:27 +0530

    small

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,10 @@ const nextConfig = {
           protocol: 'https',
           hostname: 'img.clerk.com'
         },
+        {
+          protocol: 'https',
+          hostname: 'exuberant-anteater-223.convex.cloud'
+        },
       ]
     }
   };
